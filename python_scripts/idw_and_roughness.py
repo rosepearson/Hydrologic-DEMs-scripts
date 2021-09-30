43fe15418224ae89a49effe02af497fcedd0ea51
Rose Pearson <Rose.Pearson@niwa.co.nz>
2021-09-30 16:30:05 +1300

    Added function calculating a friction related value as suggested by Graham

--- python_scripts/idw_and_roughness.py
+++ python_scripts/idw_and_roughness.py
@@ -48,9 +48,11 @@ def idw_function(xy_in: numpy.ndarray, z_in: numpy.ndarray, xy_out: numpy.ndarra
 
     return z_out
 
-def roughness_function(xy_in: numpy.ndarray, z_in: numpy.ndarray, xy_out: numpy.ndarray, search_radius: float,
-                 ground_elevations: numpy.ndarray, voxel_heights: numpy.ndarray = numpy.asarray([0, 0.5, 1, 1.5, 2]),
-                 leafsize: int = 10, eps: float = 0):
+
+def roughness_function_mewis(xy_in: numpy.ndarray, z_in: numpy.ndarray, xy_out: numpy.ndarray, search_radius: float,
+                             ground_elevations: numpy.ndarray,
+                             voxel_heights: numpy.ndarray = numpy.asarray([0, 0.5, 1, 1.5, 2]), leafsize: int = 10,
+                             eps: float = 0):
     assert len(xy_in) == len(z_in), f"len(X) of {len(xy_in)} != len(z_in) {len(z_in)}"
     assert type(z_in) is numpy.ndarray and z_in.ndim == 1, f"Expecting z_in to be a 1D numpy array instead got {z_in}"
     assert type(xy_out) is numpy.ndarray and xy_out.ndim == 2, "Expect xy_out to be a 2D numpy array " \
@@ -62,7 +64,7 @@ def roughness_function(xy_in: numpy.ndarray, z_in: numpy.ndarray, xy_out: numpy.
     z_out = numpy.empty((len(xy_out), len(voxel_heights) - 1))
 
     for i, (near_indicies, point) in enumerate(zip(tree_index_list, xy_out)):
-        
+     
         # count number of points in each height
         near_indicies = numpy.array(near_indicies)
         near_heights = z_in[near_indicies]
@@ -73,11 +75,38 @@ def roughness_function(xy_in: numpy.ndarray, z_in: numpy.ndarray, xy_out: numpy.
 
     return z_out
 
-def setup_pdal(h_crs, v_crs, verbose=True):
-    
-    if(verbose):
-        print(f"Loading in Lidar Tile")
+def roughness_function_graham(xy_in: numpy.ndarray, z_in: numpy.ndarray, xy_out: numpy.ndarray, search_radius: float,
+                              ground_elevations: numpy.ndarray,
+                              voxel_heights: numpy.ndarray = numpy.asarray([0, 0.5, 1, 1.5, 2]), leafsize: int = 10,
+                              eps: float = 0):
+    assert len(xy_in) == len(z_in), f"len(X) of {len(xy_in)} != len(z_in) {len(z_in)}"
+    assert type(z_in) is numpy.ndarray and z_in.ndim == 1, f"Expecting z_in to be a 1D numpy array instead got {z_in}"
+    assert type(xy_out) is numpy.ndarray and xy_out.ndim == 2, "Expect xy_out to be a 2D numpy array " \
+        f" instead got {xy_out}"
+    assert voxel_heights[-1] > voxel_heights[0], "The voxel heights must be monotonically increasing"
 
+    tree = scipy.spatial.KDTree(xy_in, leafsize=leafsize)  # build the tree
+    tree_index_list = tree.query_ball_point(xy_out, r=search_radius, eps=eps)
+    z_out = numpy.empty((len(xy_out), len(voxel_heights) - 1))
+
+    for i, (near_indicies, point) in enumerate(zip(tree_index_list, xy_out)):
+     
+        # count number of points in each height
+        near_indicies = numpy.array(near_indicies)
+        near_heights = z_in[near_indicies]
+        count_below_height = numpy.array([len(near_indicies[near_heights < threshold_elevation])
+                                          for threshold_elevation in voxel_heights + ground_elevations[i]])
+        z_out[i] = (count_below_height[1:] - count_below_height[:-1]) / count_below_height[1:] \
+            * (voxel_heights[1:] - voxel_heights[:-1])
+
+    return z_out
+
+
+def setup_pdal(h_crs, v_crs, verbose=True):
+
+    if(verbose):
+        print("Loading in Lidar Tile")
+    
     LAS_GROUND = 2
     tile_name = pathlib.Path(
         r"C:\Users\pearsonra\Documents\data\test_parallel\cache\Wellington_2013\ot_CL1_WLG_2013_1km_083039.laz")
@@ -157,24 +186,55 @@ def main_compare():
     voxel_height = 0.5
     dim_z = numpy.arange(0, 2, voxel_height) + voxel_height / 2
     voxel_heights = numpy.arange(0, 2 + voxel_height, voxel_height)
-    grid_dem_z = numpy.empty((len(dim_z), len(dim_y), len(dim_x)), dtype=raster_type)
-    roughness = xarray.DataArray(grid_dem_z, coords={'z': dim_z, 'y': dim_y, 'x': dim_x}, dims=['z', 'y', 'x'],
+    grid_dem_r = numpy.empty((len(dim_z), len(dim_y), len(dim_x)), dtype=raster_type)
+    roughness = xarray.DataArray(grid_dem_r, coords={'z': dim_z, 'y': dim_y, 'x': dim_x}, dims=['z', 'y', 'x'],
                                  attrs={'scale_factor': 1.0, 'add_offset': 0.0, 'long_name': 'vegetation density'})
     roughness.rio.write_crs(h_crs, inplace=True)
     roughness.name = 'w'
     roughness = roughness.rio.write_nodata(numpy.nan)
     roughness.data[0] = numpy.nan
+    roughness_gs = roughness.copy(deep=True)
 
-    # Python roughness - function
+    # Python roughness - function Mewis
     start_time = time.time()
-    z_out_flat = roughness_function(xy_in, z_in, grid_xy, ground_elevations=z_out_flat, voxel_heights=voxel_heights,
-                                    leafsize=leafsize, search_radius=radius, eps=eps)
-    z_out = z_out_flat.transpose().reshape(grid_dem_z.shape)
+    rough_flat = roughness_function_mewis(xy_in, z_in, grid_xy, ground_elevations=z_out_flat, voxel_heights=voxel_heights,
+                                          leafsize=leafsize, search_radius=radius, eps=eps)
+    r_out = rough_flat.transpose().reshape(grid_dem_r.shape)
     end_time = time.time()
     rough_time = end_time-start_time
     if(verbose):
-        print(f"Roughness function takes {rough_time}")
-    roughness.data[0] = z_out
+        print(f"Roughness function mewis takes {rough_time}")
+    roughness.data = r_out
+
+    # Python roughness - function Mewis
+    start_time = time.time()
+    rough_flat = roughness_function_graham(xy_in, z_in, grid_xy, ground_elevations=z_out_flat, voxel_heights=voxel_heights,
+                                           leafsize=leafsize, search_radius=radius, eps=eps)
+    r_out = rough_flat.transpose().reshape(grid_dem_r.shape)
+    end_time = time.time()
+    rough_time = end_time-start_time
+    if(verbose):
+        print(f"Roughness function graham takes {rough_time}")
+    roughness_gs.data = r_out
+
+    if(verbose):
+        print("Plot results")
+    vmin = roughness.data.min()
+    vmax = roughness.data.max()
+    roughness.isel(z=0).plot(vmin=vmin, vmax=vmax)
+    roughness.isel(z=1).plot(vmin=vmin, vmax=vmax)
+    roughness.isel(z=2).plot(vmin=vmin, vmax=vmax)
+    roughness.isel(z=3).plot(vmin=vmin, vmax=vmax)
+    dem.plot()
+    vmin = max(vmin, 0.01)
+    roughness.isel(z=0).plot(vmin=vmin, vmax=vmax, norm=matplotlib.colors.LogNorm())
+    roughness.isel(z=1).plot(vmin=vmin, vmax=vmax, norm=matplotlib.colors.LogNorm())
+    roughness.isel(z=2).plot(vmin=vmin, vmax=vmax, norm=matplotlib.colors.LogNorm())
+    roughness.isel(z=3).plot(vmin=vmin, vmax=vmax, norm=matplotlib.colors.LogNorm())
+    roughness_gs.isel(z=0).plot()
+    roughness_gs.isel(z=1).plot()
+    roughness_gs.isel(z=2).plot()
+    roughness_gs.isel(z=3).plot()
 
 if __name__ == "__main__":
     main_compare()
